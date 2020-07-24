69cbb9fc10ef89d55e673390054e54afb28ad697
Al-Baraa <a.elhag01@gmail.com>
2020-07-24 18:13:57 -0400

    Starting working on a_vs/ev.py

---
+++ a_vs/ev.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import loadmat
+
+## Part1: Loading EV Data
+
+data_ev = loadmat('1DataEV.mat')
+"""
+Dict names:
+
+Logic710_hr
+Logic36_hr
+Logic89_hr
+Logic1_hr
+Logic11_hr
+Logic801_hr
+
+"""
