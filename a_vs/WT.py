601300260ab2aa797cb4f701a87435d2fb52597e
Al-Baraa <a.elhag01@gmail.com>
2020-07-24 17:27:40 -0400

    Added edges for a_vs/WT.py

--- a_vs/WT.py
+++ a_vs/WT.py
@@ -16,28 +16,49 @@ Edges_WT = [0 4:1.5:16 25 100]
 PowEdg_WT = discretize(Wind_Yr,Edges_WT)
 """
 
+## Part1: Inputting Wind Data
 data = loadmat('1DataOrig.mat')
-Wind = data['Wind']
+wind = data['Wind']
+wind = wind.reshape((10, 8760))
 
-edges_wt = np.r_[0, 4:17:1.5, 25, 100] # shape(12,)
+## Part2: Edging
+edges_wt = np.r_[0, 4:16.5:1.5, 25, 100] # shape(12,)
+
 """
+00 0.0  ==>  00 - 01 0.0
+01 4.0  ==>  01 - 02 (4.0+5.5)/2
+02 5.5  ==>  02 - 03 (5.5+7.0)/2
+03 7.0  ==>  03 - 04 (7.0+8.5)/2
+04 8.5  ==>  04 - 05 (8.5+10.0)/2
+05 10.0 ==>  05 - 06 (10.0+11.5)/2
+06 11.5 ==>  06 - 07 (11.5+13.0)/2
+07 13.0 ==>  07 - 08 (13.0+14.5)/2
+08 14.5 ==>  08 - 09 (14.5+16.0)/2
+09 16.0 ==>  09 - 10 16.0 # Rated Speed
+10 25.0 ==>  10 - 11 0.0 # Cut_Out Speed
+11 0.0
+----
 
-00 0.0  =>  00 0.0
-01 4.0  =>  01 (4.0+5.5)/2
-02 5.5  =>  02 (5.5+7.0)/2
-03 7.0  =>  03 (7.0+8.5)/2
-04 8.5  =>  04 (8.5+10.0)/2
-05 10.0 =>  05 (10.0+11.5)/2
-06 11.5 =>  06 (11.5+13.0)/2
-07 13.0 =>  07 (13.0+14.5)/2
-08 14.5 =>  08 (14.5+16.0)/2
-09 16.0 =>  09 16.0
-10 25.0 =>  10 25.0
-11 0.0  =>  11 0.0
+Added :
+pow_edges_wt = np.r_[0, pow_edges_wt]
+
+First is useless
 """
-pow_edges_wt = edges_wt # Power Edges to simulate WT
-
+pow_edges_wt = np.copy(edges_wt)
+pow_edges_wt = pow_edges_wt[:-1] # Power Edges to simulate WT
 pow_edges_wt[-1] = 0
-pow_edges_wt[-2] = 16
-for count, _ in enumerate(range(1,edges_wt.shape[0]-2)):
-    print(count+1, pow_edges_wt[_])
+for _ in range(1,pow_edges_wt.shape[0]-2):
+    pow_edges_wt[_] += pow_edges_wt[_+1]
+    pow_edges_wt[_] /= 2
+
+pow_edges_wt = np.r_[0, pow_edges_wt]
+
+## Part3: Applying Edges
+wind_edges = np.digitize(wind, edges_wt)
+wind_edges = pow_edges_wt[wind_edges]
+
+## Part4: Plotting
+plt.plot(wind_edges[0,:])
+plt.show()
+
+
