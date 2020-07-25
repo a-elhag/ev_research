da49ab20f0211b8d6c366ffb8067fcee122b99bc
Al-Baraa <a.elhag01@gmail.com>
2020-07-24 20:13:26 -0400

    Same as before

--- a_vs/ev.py
+++ a_vs/ev.py
@@ -3,16 +3,25 @@ import numpy as np
 from scipy.io import loadmat
 
 ## Part1: Loading EV Data
-
 data_ev = loadmat('1DataEV.mat')
-"""
-Dict names:
-
-Logic710_hr
-Logic36_hr
-Logic89_hr
-Logic1_hr
-Logic11_hr
-Logic801_hr
 
 """
+for key, value in data_ev.items():
+    if type(value).__module__ == np.__name__:
+        print(key, value.shape)
+
+Logic710_hr (1, 366)
+Logic36_hr (1, 366)
+Logic89_hr (1, 366)
+Logic1_hr (1, 366)
+Logic11_hr (1, 366)
+Logic801_hr (1, 366)
+
+!Indexing for Dummies!
+data_ev['LogicXXX_hr'][0, day][driver, hour]
+"""
+
+## Part2: Arrival Locations + Duration
+# We need to find when our drivers arrive and for how long they stay
+
+a = data_ev['Logic710_hr'][0, 0]
