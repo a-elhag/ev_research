543dd8c7a2a92fb20679788b0914377e7cebad5c
Al-Baraa <a.elhag01@gmail.com>
2020-07-25 17:55:50 -0400

    huzzah, we split for PV and WT

--- c_mc/split.py
+++ c_mc/split.py
@@ -52,12 +52,25 @@ class Split():
         if self.ev_flag:
             pass
         else:
-            pass
+            for _ in range(4):
+                self.seasons[_] = self.data.take(self.seasons_range[_],
+                                                 axis=1, mode='wrap')
 
 
 split_pv = Split(Ppv, False)
 split_pv.season_range()
+split_pv.season_split()
 
-for _ in range(4):
-    print(split_pv.seasons_range[_])
+import matplotlib.pyplot as plt
+fig = np.empty(4, object)
+ax = np.empty(4, object)
 
+for season in range(4):
+    fig[season] = plt.figure()
+    ax[season] = plt.axes()
+    for year in range(6):
+        ax[season].plot(split_pv.seasons[season][year,:], label=f"{year}")
+        ax[season].set_title(f"Season Number {season}")
+        ax[season].legend()
+
+plt.show()
