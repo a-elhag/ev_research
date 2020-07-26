aa59d5c9e2605356891801a78b13ad486635352f
Al-Bara El-Hag <a.elhag01@gmail.com>
2020-07-26 18:49:26 -0400

    Made a big whoopsy by using break instead of continue

--- b_vs/ev_clean.py
+++ b_vs/ev_clean.py
@@ -5,24 +5,24 @@ array_ev = np.load('../a_data/b_vs/out/ev.npy', allow_pickle=True)
 # Remove the first day
 array_ev = array_ev[:, 1:]
 
-# # Remove all zeros
-# for year in range(6):
-#     for day in range(1):
-#         if year>0:
-#             break
-#         print(array_ev[year, day])
 
 rem_count=0
 total_count=0
 for year in range(array_ev.shape[0]):
     for day in range(array_ev.shape[1]):
+        print(year, day)
         if array_ev[year, day].ndim == 1:
-            break
+            continue
+
+        if year==0 and day==array_ev[year, day].shape[1]-10:
+            print(array_ev[year, day])
 
         shape1 = array_ev[year, day].shape[0]
         remove_hours = [0, 1, 2, 3, 4, 22, 23, 24]
         for remove in remove_hours:
             array_ev[year, day] = array_ev[year, day][array_ev[year, day][:,0] != remove]
+        if year==0 and day==array_ev[year, day].shape[1]-10:
+            print(array_ev[year, day])
         shape2 = array_ev[year, day].shape[0]
         total_count += shape2
 
--- c_mc/split.py
+++ c_mc/split.py
@@ -8,7 +8,7 @@ Pwt = np.load('../a_data/b_vs/out/wt.npy')
 class Split():
     """ Split data to seasons, hours and/or weekends/days """
 
-    def __init__(self, data, ev_flag):
+    def __init__(self, data, ev_flag = False):
         self.data = data
         self.ev_flag = ev_flag
 
@@ -39,18 +39,27 @@ class Split():
         self.day1 = self.day1.days + 1
         self.hour1 = self.day1*24
 
-        self.seasons_range = np.empty((4), object)
-        self.seasons_range[0] = np.array(range(self.hour1, self.hour_no(1)))
-        self.seasons_range[1] = np.array(range(self.hour_no(1), self.hour_no(2)))
-        self.seasons_range[2] = np.array(range(self.hour_no(2), self.hour_no(3)))
-        self.seasons_range[3] = np.array(range(self.hour_no(3), self.hour_no(4)))
+        if self.ev_flag:
+            self.seasons_range = np.empty((4), object)
+            self.seasons_range[0] = np.array(range(self.day1, self.day_no(1)))
+            self.seasons_range[1] = np.array(range(self.day_no(1), self.day_no(2)))
+            self.seasons_range[2] = np.array(range(self.day_no(2), self.day_no(3)))
+            self.seasons_range[3] = np.array(range(self.day_no(3), self.day_no(4)))
+        else:
+            self.seasons_range = np.empty((4), object)
+            self.seasons_range[0] = np.array(range(self.hour1, self.hour_no(1)))
+            self.seasons_range[1] = np.array(range(self.hour_no(1), self.hour_no(2)))
+            self.seasons_range[2] = np.array(range(self.hour_no(2), self.hour_no(3)))
+            self.seasons_range[3] = np.array(range(self.hour_no(3), self.hour_no(4)))
 
 
     def season_split(self):
         self.seasons = np.empty((4), object)
 
         if self.ev_flag:
-            pass
+            for _ in range(4):
+                self.seasons[_] = self.data.take(self.seasons_range[_],
+                                                 axis=1, mode='wrap')
         else:
             for _ in range(4):
                 self.seasons[_] = self.data.take(self.seasons_range[_],
@@ -70,14 +79,17 @@ class Split():
 
 
 
-split_wt = Split(Pwt, False)
+split_wt = Split(Pwt)
 split_wt.season_range()
 split_wt.season_split()
 split_wt.hour_split()
 
-split_pv = Split(Ppv, False)
+split_pv = Split(Ppv)
 split_pv.season_range()
 split_pv.season_split()
 split_pv.hour_split()
 
+split_ev = Split(array_ev, True)
+split_ev.season_range()
+split_ev.season_split()
 
