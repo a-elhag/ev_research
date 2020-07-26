0087e3ae597c7b2461d8064ca7934e36a007d7fa
Al-Bara El-Hag <a.elhag01@gmail.com>
2020-07-26 19:49:48 -0400

    Set up plotting and saving, almost done but saving this just in case

--- b_vs/ev_plot.py
+++ b_vs/ev_plot.py
@@ -9,25 +9,26 @@ array_ev = np.load('../a_data/b_vs/out/ev.npy', allow_pickle = True)
 def what_day(day):
     new_year = dt.date(2012, 1, 1)
     day_now = new_year + dt.timedelta(days=day)
-    return calendar.day_name[day_now.weekday()]
+    return day_now.__str__(), calendar.day_name[day_now.weekday()]
 
 
-fig = np.empty(20, object)
-ax = np.empty(20, object)
+fig = np.empty(30, object)
+ax = np.empty(30, object)
 
 year = 0
-for c, day in enumerate(range(10, 30)):
+for c, day in enumerate(range(0, 30)):
     fig[c] = plt.figure()
     ax[c] = plt.axes()
     if array_ev[0, day].ndim ==1:
         continue
 
-    day_name = what_day(day)
+    date_num, day_name = what_day(day)
 
     x = np.arange(array_ev[year, day].shape[0])
     ax[c].scatter(x, array_ev[year, day][:,0], label='Arrival')
     ax[c].scatter(x, array_ev[year, day][:,0] + array_ev[year, day][:,1], label='Duration')
-    ax[c].set_title(f"Year: {year}, Day: {day_name}")
+    ax[c].set_title(f"Year: {year}, Day: {day_name}, {date_num}")
     ax[c].legend()
+    fig[c].savefig(f'../a_data/b_vs/plots/Y{year}-D{day}.png', dpi=300, bbox_inches='tight')
+    plt.close(fig[c])
 
-# plt.show()
