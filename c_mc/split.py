ff77c9584f2a54d41b707b110a808c897dae60e8
Al-Bara El-Hag <a.elhag01@gmail.com>
2020-07-26 15:43:53 -0400

    Loaded the ev_clean.npy

--- c_mc/split.py
+++ c_mc/split.py
@@ -1,7 +1,7 @@
 import datetime as dt
 import numpy as np
 
-array_ev = np.load('../a_data/b_vs/out/ev.npy', allow_pickle=True)
+array_ev = np.load('../a_data/b_vs/out/ev_clean.npy', allow_pickle=True)
 Ppv = np.load('../a_data/b_vs/out/pv.npy')
 Pwt = np.load('../a_data/b_vs/out/wt.npy')
 
