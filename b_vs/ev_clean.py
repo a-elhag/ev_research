44669ac6e3877b22c4f3aa38303c4e71dc016c27
Al-Bara El-Hag <a.elhag01@gmail.com>
2020-07-26 14:55:47 -0400

    Removed any car that arrived at zero

--- b_vs/ev_clean.py
+++ b_vs/ev_clean.py
@@ -2,8 +2,30 @@ import numpy as np
 
 array_ev = np.load('../a_data/b_vs/out/ev.npy', allow_pickle=True)
 
-print(array_ev.shape)
-
 # Remove the first day
 array_ev = array_ev[:, 1:]
+
+# # Remove all zeros
+# for year in range(6):
+#     for day in range(1):
+#         if year>0:
+#             break
+#         print(array_ev[year, day])
+
+rem_count=0
+total_count=0
+for year in range(array_ev.shape[0]):
+    for day in range(array_ev.shape[1]):
+        if array_ev[year, day].ndim == 1:
+            break
+        shape1 = array_ev[year, day].shape[0]
+        array_ev[year, day] = array_ev[year, day][array_ev[year, day][:,0] != 0]
+        shape2 = array_ev[year, day].shape[0]
+        total_count += shape2
+
+        if shape2 != shape1:
+            rem_count += shape1-shape2
+
+print(rem_count)
+print(total_count)
 
