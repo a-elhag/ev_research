ecebdf13c8a81c3363a1bfafecdeb2a9a10b3585
Al-Bara El-Hag <a.elhag01@gmail.com>
2020-07-26 21:16:40 -0400

    Locations and size of the parking lots

--- b_vs/ev_plot.py
+++ b_vs/ev_plot.py
@@ -12,23 +12,23 @@ def what_day(day):
     return day_now.__str__(), calendar.day_name[day_now.weekday()]
 
 
-fig = np.empty(30, object)
-ax = np.empty(30, object)
+fig = np.empty((6, 366), object)
+ax = np.empty((6, 366), object)
 
-year = 0
-for c, day in enumerate(range(0, 30)):
-    fig[c] = plt.figure()
-    ax[c] = plt.axes()
-    if array_ev[0, day].ndim ==1:
-        continue
+for lot in range(array_ev.shape[0]):
+    for day in range(array_ev.shape[1]):
+        fig[lot, day] = plt.figure()
+        ax[lot, day] = plt.axes()
+        if array_ev[lot, day].ndim ==1:
+            continue
 
-    date_num, day_name = what_day(day)
+        date_num, day_name = what_day(day)
 
-    x = np.arange(array_ev[year, day].shape[0])
-    ax[c].scatter(x, array_ev[year, day][:,0], label='Arrival')
-    ax[c].scatter(x, array_ev[year, day][:,0] + array_ev[year, day][:,1], label='Duration')
-    ax[c].set_title(f"Year: {year}, Day: {day_name}, {date_num}")
-    ax[c].legend()
-    fig[c].savefig(f'../a_data/b_vs/plots/Y{year}-D{day}.png', dpi=300, bbox_inches='tight')
-    plt.close(fig[c])
+        x = np.arange(array_ev[lot, day].shape[0])
+        ax[lot, day].scatter(x, array_ev[lot, day][:,0], label='Arrival')
+        ax[lot, day].scatter(x, array_ev[lot, day][:,0] + array_ev[lot, day][:,1], label='Duration')
+        ax[lot, day].set_title(f"Lot: {lot}, Day: {day_name}, {date_num}")
+        ax[lot, day].legend()
+        fig[lot, day].savefig(f'../a_data/b_vs/plots/L{lot}-D{day}.png', dpi=150, bbox_inches='tight')
+        plt.close(fig[lot, day])
 
