cb9800565c9165bd348f8cc926a07ff14c1ab3b1
Al-Bara El-Hag <a.elhag01@gmail.com>
2020-07-29 18:55:12 -0400

    split weekday and weekend, next is by 24 hours

--- monte/split_ev.py
+++ monte/split_ev.py
@@ -4,15 +4,10 @@ import numpy as np
 
 array_ev_clean = np.load('../data/preprocessing/ev_clean.npy', allow_pickle=True)
 
-
 wday = np.arange(0,5)
 
 split_ev = np.empty((4,1), object)
 
-for lot in range(4):
-    split_ev[lot, 0] = np.empty((1,2), object)
-    split_ev[lot, 0][0, 0] = np.empty((1, 2), object)
-    split_ev[lot, 0][0, 1] = np.empty((1, 2), object)
 
 """
 split_ev[lot, 0][0, Wday?][0, day]
@@ -39,20 +34,11 @@ def plot_ev(array, fig, ax, wday, lot, count):
     plt.close(fig)
     ax.cla()
 
-#             plot_ev(split_ev[lot, 0][0, 0][0, wday_count], \
-#                     fig, ax, True, lot, wday_count)
-#             plot_ev(split_ev[lot, 0][0, 1][0, wend_count], \
-#                     fig, ax, False, lot, wend_count)
-
 for lot in range(4):
     day_count=0
     date_idx = dt.date(2012, 1, 1)
-    list_day_arr = []
-    list_day_dur = []
-    list_end_arr = []
-    list_end_dur = []
+    list_day_arr = []; list_day_dur = []; list_end_arr = []; list_end_dur = []
     while date_idx.year != 2013:
-        print(date_idx)
         # Return day of the week, where Monday == 0 ... Sunday == 6
         if array_ev_clean[lot, day_count].size == 0:
             day_count += 1
@@ -67,3 +53,10 @@ for lot in range(4):
         day_count += 1
         date_idx += dt.timedelta(days=1)
 
+for lot in range(4):
+    split_ev[lot, 0] = np.empty((1,2), object)
+
+    split_ev[lot, 0][0, 0] = np.array(list_day_arr)
+    split_ev[lot, 0][0, 1] = np.array(list_day_dur)
+    split_ev[lot, 0][0, 0] = np.array(list_end_arr)
+    split_ev[lot, 0][0, 1] = np.array(list_end_dur)
