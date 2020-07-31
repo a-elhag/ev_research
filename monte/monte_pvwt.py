7a889b1d45c284eaf1f6b94e6bca3f53bfc48bff
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-07-31 03:03:43 -0400

    icdf

--- monte/monte_pvwt.py
+++ monte/monte_pvwt.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from split_pvwt import SplitRenewables
+import time
 
 Ppv = np.load('../data/preprocessing/pv.npy')
 Pwt = np.load('../data/preprocessing/wt.npy')
@@ -14,6 +15,20 @@ split_wt.run() # split_wt.data_out
 bins_pv = np.empty((4,24), object)
 bins_wt = np.empty((4,24), object)
 
-pdf, bin_edges = np.histogram(split_pv.data_out[0,0]) # Density normalizes
-pdf = pdf/pdf.sum()
-cdf = np.cumsum(pdf)
+def timing(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time() - start_time
+        print(end_time)
+        return end_time
+    return wrapper
+
+@timing
+def simple_icdf(x):
+    for _ in range(x):
+        y = np.random.rand(1)
+        np.quantile(split_pv.data_out[2,10], y)
+
+
+simple_icdf(10*8760)
