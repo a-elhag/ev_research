c5dd3594afeab14b7acabc002b17262bf61f8e65
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-08-01 16:39:30 -0400

    preprocessing is now run in main.py

--- preprocessing.py
+++ preprocessing.py
@@ -179,27 +179,3 @@ class WT:
 
     def save(self, out_location):
         np.save(out_location, self.Pwt)
-
-
-ev_in = 'data/in/parking_lot.mat'
-ev_out = 'data/preprocessing/ev.npy'
-
-my_ev = EV(ev_in)
-my_ev.run()
-my_ev.save(ev_out)
-
-
-pv_in = 'data/in/solar_wind.mat'
-pv_out = 'data/preprocessing/pv.npy'
-
-my_pv = PV(pv_in)
-my_pv.run()
-my_pv.save(pv_out)
-
-
-wt_in = 'data/in/solar_wind.mat'
-wt_out = 'data/preprocessing/wt.npy'
-
-my_wt = WT(wt_in)
-my_wt.run()
-my_wt.save(wt_out)
