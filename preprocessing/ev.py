8f3347d1ab2d8cba2087df283b00e2c70b2cd5c8
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-08-01 16:17:36 -0400

    classified the function

--- preprocessing/ev.py
+++ preprocessing/ev.py
@@ -1,56 +1,65 @@
 import numpy as np
-import pdb
 from scipy.io import loadmat
 
-## Part1: Loading EV Data
-data_ev = loadmat('../data/in/parking_lot.mat')
+class EV():
+    def __init__(self, data_location):
+        """
+        for key, value in data_ev.items():
+            if type(value).__module__ == np.__name__:
+                print(key, value.shape)
 
-"""
-for key, value in data_ev.items():
-    if type(value).__module__ == np.__name__:
-        print(key, value.shape)
+        Logic710_hr (1, 366)
+        Logic36_hr (1, 366)
+        Logic89_hr (1, 366)
+        Logic1_hr (1, 366)
+        Logic11_hr (1, 366)
+        Logic801_hr (1, 366)
 
-Logic710_hr (1, 366)
-Logic36_hr (1, 366)
-Logic89_hr (1, 366)
-Logic1_hr (1, 366)
-Logic11_hr (1, 366)
-Logic801_hr (1, 366)
+        !Indexing for Dummies!
+        data_ev['LogicXXX_hr'][0, day][driver, hour]
+        """
+        self.data = loadmat(data_location)
 
-!Indexing for Dummies!
-data_ev['LogicXXX_hr'][0, day][driver, hour]
-"""
+    def parsing(self):
+        """
+        We need to find when our drivers arrive and for how long they stay
 
-## Part2: Arrival Locations + Duration
-# We need to find when our drivers arrive and for how long they stay
-array_ev = np.empty((6, 366), object)
+        self.ev_parse[parking_lot, day][driver, 0] ==> Arrival
+        self.ev_parse[parking_lot, day][driver, 1] ==> Duration
+        """
+        self.ev_parse = np.empty((6, 366), object)
 
-idx = -1
-for key, lot in data_ev.items():
-    if type(lot).__module__ == np.__name__:
-        idx += 1
-        for day in range(lot.shape[1]):
-            list_ev = []
-            lot_day = lot[0, day]
+        idx = -1
+        for key, lot in self.data.items():
+            if type(lot).__module__ == np.__name__:
+                idx += 1
+                for day in range(lot.shape[1]):
+                    list_ev = []
+                    lot_day = lot[0, day]
 
-            for lot_day_row in lot_day:
-                try:
-                    idx_row = list(lot_day_row).index(1)
-                    sum_row = np.sum(lot_day_row)
-                    list_ev.append([idx_row, sum_row])
-                except:
-                    break
-            array_ev[idx, day] = np.array(list_ev)
+                    for lot_day_row in lot_day:
+                        try:
+                            idx_row = list(lot_day_row).index(1)
+                            sum_row = np.sum(lot_day_row)
+                            list_ev.append([idx_row, sum_row])
+                        except:
+                            break
+                    self.ev_parse[idx, day] = np.array(list_ev)
 
+    def clean(self):
+        self.lot_keep = np.array([0, 1, 3, 4])
+        self.ev_clean = self.ev_parse[self.lot_keep, :]
 
-np.save('../data/preprocessing/ev.npy', array_ev)
-"""
-array_ev[parking_lot, day][driver, 0] ==> Arrival
-array_ev[parking_lot, day][driver, 1] ==> Duration
-"""
+    def run(self):
+        self.parsing()
+        self.clean()
 
-# Cleaning
-idx = np.array([0, 1, 3, 4])
-array_ev_clean = array_ev[idx, :]
+    def save(self, out_location):
+        np.save(out_location, self.ev_clean)
 
-np.save('../data/preprocessing/ev_clean.npy', array_ev_clean)
+data_location = '../data/in/parking_lot.mat'
+out_location = '../data/preprocessing/ev.npy'
+
+my_ev = EV(data_location)
+my_ev.run()
+my_ev.save(out_location)
