178200ccc8af954d76347d6c1f2868ddc5ea2eba
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-08-17 08:11:37 -0400

    import SplitRenewables

--- src/generate.py
+++ src/generate.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from split_pvwt import SplitRenewables
 from sql_numpy import SQL_Numpy
 
 pv_data = np.load('data/preprocessing/pv.npy', allow_pickle=True)
 
+split_pv = SplitRenewables(pv_data)
+split_pv.run()
 # pv_sql = SQL_Numpy('pv.db')
+
