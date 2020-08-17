bea4c24a286162075e257fc1d26cc8473b5552dc
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-08-16 21:03:08 -0400

    Generate virtual scenarios

---
+++ monte/generate.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sql_numpy import SQL_Numpy
+
+pv_data = np.load('data/preprocessing/pv.npy', allow_pickle=True)
+
+# pv_sql = SQL_Numpy('pv.db')
