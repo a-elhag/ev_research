4175e9cea2b712aea1cb2797345a029dc914a12a
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-08-18 11:51:41 -0400

    added generator

--- src/generate.py
+++ src/generate.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt
+from monte_pvwt import full_icdf
 import numpy as np
 from split_pvwt import SplitRenewables
 from sql_numpy import SQL_Numpy
 
 pv_data = np.load('data/preprocessing/pv.npy', allow_pickle=True)
 
-split_pv = SplitRenewables(pv_data)
-split_pv.run()
-# pv_sql = SQL_Numpy('pv.db')
+pv_split = SplitRenewables(pv_data)
+pv_split.run()
 
+pv_split = SplitRenewables(pv_data)
+pv_split.run()
+#pv_split.data_out
+
+pv_gen = full_icdf(pv_split.data_out, 20)
