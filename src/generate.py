8f8d82ff1a266391cbdbc5204d5d18c603f98203
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-08-22 03:54:14 -0400

    set up generating class

--- src/generate.py
+++ src/generate.py
@@ -5,30 +5,39 @@ import numpy as np
 from split_pvwt import SplitRenewables
 from sql_numpy import SQL_Numpy
 
-## Part 2: Loading Data
-pv_data = np.load('data/preprocessing/pv.npy', allow_pickle=True)
+## Part 2: Making Class
+class Generate():
+    def __init__(self, data_loc, db_loc):
+        self.db_loc = db_loc
+        self.data_loc = data_loc
+        self.data_in = np.load(data_loc, allow_pickle=True)
+        self.data_in_split = SplitRenewables(self.data_in)
+        self.data_in_split.run() # pv_split.data_out
 
-pv_split = SplitRenewables(pv_data)
-pv_split.run()
+    def sql_connect(self):
+        self.sql = SQL_Numpy(self.db_loc)
+        self.sql.connect()
 
-pv_split = SplitRenewables(pv_data)
-pv_split.run() # pv_split.data_out
+    def sql_commit_close(self):
+        self.sql.commit()
+        self.sql.close()
 
-## Part 3: Saving Data into Database
-pv_sql = SQL_Numpy('db_pv.db')
-pv_sql.connect()
-for repeat in range(4):
-    pv_gen = full_icdf(pv_split.data_out, repeat)
-    pv_sql.insert(pv_gen)
+    def monte(self, years):
+        self.sql_connect()
+        self.data_gen = full_icdf(self.data_in_split.data_out, years)
+        self.sql.insert(self.data_gen)
+        self.sql_commit_close()
 
-pv_sql.commit()
-pv_sql.close()
+    def yank(self):
+        self.sql_connect()
+        self.sql.first_select()
+        self.data_out = self.sql.data
+        self.sql.first_delete()
+        self.sql_commit_close()
 
-
-## Part 4: Saving Data into Database
-pv_sql.connect()
-pv_sql.first_delete()
-pv_sql.first_delete()
-
-pv_sql.commit()
-pv_sql.close()
+## Part 3: Running
+if __name__ == "__main__":
+    pv_gen = Generate('data/preprocessing/pv.npy', 'data/db/pv.db')
+    for year in range(1, 5):
+        pv_gen.monte(year)
+    pv_gen.yank()
