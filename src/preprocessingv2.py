af3a815fb83551b35c1acbcd80ccd7a65de3a874
Al-Baraa El-Hag <a.elhag01@gmail.com>
2021-02-17 19:48:17 -0500

    data in

--- src/preprocessingv2.py
+++ src/preprocessingv2.py
@@ -1,3 +1,17 @@
 ## Part 0: Intializing
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+## Part 1: Data In
+base = 'data/in/toronto/'
+ext = '.csv'
+years = np.arange(1998, 2019)
+
+loc = base + str(years[0]) + ext
+
+cols = ['Year', 'Month', 'Day', 'Hour', 'Minute', 'GHI',
+        'Cloud Type', 'Wind Speed', 'Temperature']
+data = pd.read_csv(loc, skiprows=2, low_memory=False, usecols = cols)
+
+
